b6e89f74257767d73a257870968e1d88e8962754
keepcalmandcodecodecode <easy.dev@yandex.ru>
2016-07-08 00:11:01 +0300

    initial

---
+++ CoreDataExample/CoreDataExample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CoreDataExample
+//
+//  Created by macmini1 on 08.07.16.
+//  Copyright © 2016 easy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
